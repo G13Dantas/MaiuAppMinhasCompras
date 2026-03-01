6c169a5e4bd78a98861445a0a795dd41d1a72868
G13Dantas <gdantasestudos01@gmail.com>
2026-03-01 19:57:54 -0300

    Alterações agenda 01

--- MaiuAppMinhasCompras/Models/Produto.cs
+++ MaiuAppMinhasCompras/Models/Produto.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace MaiuAppMinhasCompras.Models
 {
-    internal class Produto
+    public class Produto
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public int Preço { get; set; }
+
     }
+    /*Esta é a primeira pasta que foi alterada, coloquei o usingSQlite, depois criei a classe Produto, 
+         * e dentro dela coloquei as propriedades, a primeira é o Id, que é a chave primária e auto-incrementável, 
+         * depois tem a descrição, quantidade e preço.
+        Por fim cliquei com o botao direito no campo dos usings, depois em remover e classificar usos, para remover os usings que nao utilizei
+         */
 }
